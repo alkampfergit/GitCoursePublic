4ff9b3c7c0842299e0a51b4af8c661f2539ab6ad
Gian Maria Ricci <alkampfer@nablasoft.com>
2021-06-06 17:43:31 +0200

    added deflated option

--- Internals/BasicFilesOperation.ts
+++ Internals/BasicFilesOperation.ts
@@ -1,40 +1,78 @@
-import { formatWithOptions } from 'util';
 import yargs = require('yargs/yargs');
+var zlib = require('zlib');
 
 var fs = require('fs');
 
-export function getBlobHash(fileName: string): string {
+function createGitCompressedStream(fileName: string): Buffer {
+  //reading file in memory
+  const fileData: [] = fs.readFileSync(fileName);
     
-    //reading file in memory
-    console.log("Reading file", fileName);
-    const fileData: [] = fs.readFileSync(fileName);
+  //blob string directly converted in ascii
+  const header: string = "blob " + fileData.length.toString();
+  var headerBuffer = Buffer.from(header, "ascii");
+  return Buffer.concat([headerBuffer,  Buffer.from('\0'), Buffer.from(fileData)]);
+}
+
+/**
+ * Create git destination blob file in memory, write to destination folder
+ * and return the hash of the file (the name of the file.)
+ * @param fileName 
+ * @param destinationDir 
+ * @returns 
+ */
+export function addFile(fileName: string, destinationDir: string): string {
     
-    //blob string directly converted in ascii
-    const header: string = "blob " + fileData.length.toString();
-    var headerByte = Buffer.from(header, "ascii");
-    headerByte = Buffer.concat([headerByte,  Buffer.from('\0')]);
-    const rawData1: [] = fs.readFileSync("c:\\temp\\test.txt");
-    console.log("rawData1", rawData1, rawData1.length);
+    const fileContent = createGitCompressedStream(fileName);
+  
+    /*
+    // If you really want to write the file somewhere, this will be the uncompressed version of the file
     const fd = fs.openSync("c:\\temp\\test.txt",'w');
     fs.writeSync(fd, headerByte, 0, headerByte.length, null, (err: any, bytes: any, buffer: any) => {});
     fs.writeSync(fd, fileData, 0, fileData.length, null, (err: any, bytes: any, buffer: any) => {});
     fs.close(fd);
+    */
 
-    var crypto = require('crypto')
-    var shasum = crypto.createHash('sha1');
-    const rawData: [] = fs.readFileSync("c:\\temp\\test.txt");
-    console.log("rawData", rawData, rawData.length);
-    shasum.update(rawData);
-    return shasum.digest('hex');
+    const crypto = require('crypto')
+    const shasum = crypto.createHash('sha1');
+
+    shasum.update(fileContent);
+    const hash = shasum.digest('hex');
+
+    // Ok now we need to write the file into a directory
+    var deflatedContent = zlib.deflateSync(fileContent);
+    fs.writeFileSync(destinationDir + "\\" + hash, deflatedContent)
+    return hash;
+}
+
+export function inflateFile(inputFileName: string, outputFileName: string) {
+    const fileData: [] = fs.readFileSync(inputFileName);
+    const inflated = zlib.inflateSync(fileData);
+    console.log("Binary content", inflated);
+    fs.writeFileSync(outputFileName, inflated);
 }
 
 interface Arguments {
     filename: string;
+    operation: string;
+    destinationDir: string;
+    outputFile: string;
 }
 
 const argv = yargs(process.argv.slice(2)).options({
-    "filename": { type: 'string', alias: 'f', demandOption: true }
+    "operation": {choices: ['hash', 'inflate'], alias: 'o', demandOption: true},
+    "filename": { type: 'string', alias: 'f', demandOption: true },
+    "destinationDir" : {type: 'string', demandOption: false},
+    "outputFile" : {type: 'string', demandOption: false},
   }).argv as Arguments;
 
-const hash: string = getBlobHash(argv.filename);
-console.log("Hash", hash);
+switch (argv.operation) {
+    case 'hash':
+        const hash: string = addFile(argv.filename, argv.destinationDir);
+        console.log("Hash", hash)
+        break;
+    case 'inflate':
+        inflateFile(argv.filename, argv.outputFile);
+        break;
+    default:
+      console.log('operation not supported');
+  }
